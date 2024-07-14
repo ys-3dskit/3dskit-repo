f1a4faa46b5a3ed8854a4a5288411ad11743b7e3
Hazel Atkinson <yellowsink@riseup.net>
2024-07-14 15:50:18 +0100

    add 3dskit-dlang v0.1.0

--- packages/3/3dskit-dlang/xmake.lua
+++ packages/3/3dskit-dlang/xmake.lua
@@ -7,6 +7,7 @@ package("3dskit-dlang")
     "https://github.com/ys-3dskit/3dskit-dlang.git"
   )
 
+  add_versions("v0.1.0", "88be563dd3690840049e7371751fa4a4438d6b0fac3adb79765a48e1964e0cff")
   add_versions("v0.0.3", "392a677ca64abc3d8c13b0a37ee8c616ff2a6429c57db4800ad1ae07c5a7234d")
   --add_versions("v0.0.2", "baedde8c82b2410da69433dcf62c2a378482a9ff82382c1fb674afe9bbba1720")
   --add_versions("v0.0.1", "0208b2c274ef6b48affad848827882f3a50575a1918ec354f96c70866a401573")
@@ -18,6 +19,9 @@ package("3dskit-dlang")
     os.vrun("xmake")
 
     os.cp("ys3ds", package:installdir("include"))
+    os.cp("btl", package:installdir("include"))
+    os.cp("3ds-d-runtime/druntime/src", package:installdir("include"))
+    os.cp("3ds-d-runtime/phobos", package:installdir("include"))
     os.cp("build/3ds/arm/release/*", package:installdir("lib"))
   end)
 
